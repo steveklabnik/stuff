f8a71fb67534e81304698f8b86efaaac46ab14f6
Steve Klabnik <steve@steveklabnik.com>
2013-05-30 15:22:47 -0700

    Uhhh stupid changes where tests weren't running.

--- main.rs
+++ main.rs
@@ -13,10 +13,27 @@ fn hex_to_base64(source: &str) -> ~str {
   nums.to_base64()
 }
 
-#[cfg(test)]
+fn xor_sum(one: &str, two: &str) -> ~str {
+  return ~"";
+}
+
+#[test]
+fn test_xor_sum() {
+  let input1 = ~"1c0111001f010100061a024b53535009181c";
+  let input2 = ~"686974207468652062756c6c277320657965";
+
+  let expected = ~"746865206b696420646f6e277420706c6179";
+
+  assert_eq!(expected, xor_sum(input1, input2));
+}
+
+ 
+
+#[test]
 fn test_hex_to_base64() {
-  let expected = ~"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-  let outcome = hex_to_base64(expected);
+  let input = ~"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+  let expected = ~"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+  let outcome = hex_to_base64(input);
 
   assert_eq!(expected, outcome);
 }
